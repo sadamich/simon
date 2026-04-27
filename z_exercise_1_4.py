7e5b8b47d66370b26905c3f329f7371e744343ca
sadamich <sadamichikato@gmail.com>
2026-04-27 16:28:43 +0900

    Fraction

---
+++ z_exercise_1_4.py
@@ -0,0 +1,8 @@
+https://runestone.academy/ns/books/published/pythonds/Introduction/Exercises.html
+
+
+Implement the remaining relational operators
+
+(__gt__, __ge__, __lt__, __le__, and __ne__)
+
+
